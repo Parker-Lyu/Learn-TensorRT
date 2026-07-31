5a4299e0f1517d8b30534b95dc746cb11162fee0
Parker Lyu <parkerlyu01@gmail.com>
2026-07-31 15:15:59 +0800

    feat: add asynchronous TensorRT slot backend

---
+++ 21_integrated_tensorrt_video_pipeline/include/tensorrt_backend.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "pipeline_core.hpp"
+#include <opencv2/core.hpp>
+#include <cstddef>
+#include <memory>
+#include <string>
+#include <vector>
+namespace lesson21 {
+struct GpuBatchResult { BatchMetadata metadata; std::vector<float> output; std::vector<int64_t> output_shape; float preprocess_ms{}, inference_ms{}, d2h_ms{}; };
+class TensorRtBackend {
+public:
+  TensorRtBackend(const std::string& engine, std::size_t slots, cv::Size input_size);
+  ~TensorRtBackend();
+  TensorRtBackend(const TensorRtBackend&) = delete;
+  void submit(std::size_t slot, const std::vector<cv::Mat>& images, BatchMetadata metadata);
+  bool ready(std::size_t slot) const;
+  GpuBatchResult collect(std::size_t slot);
+private: struct Impl; std::unique_ptr<Impl> impl_;
+};
+}
